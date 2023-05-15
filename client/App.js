7b0dba45e61a81462d27abd0be28e078dc994d86
Võ Hoàng Tâm <vht1872002@gmail.com>
2023-05-15 15:18:16 +0700

    ok

--- client/App.js
+++ client/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Vãi Òfffsssdsfsdfsdfsssssff!</Text>
+      <Text>Vãi Òfffssssssssff!</Text>
       <StatusBar style="auto" />
     </View>
   );
