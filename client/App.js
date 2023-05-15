cb90d7ded5e21893201e80bebe9a0945d7d18fa6
williamtr4n <williamtran6111@gmail.com>
2023-05-15 15:19:28 +0700

    test

--- client/App.js
+++ client/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Vãi Òfffssssssssff!</Text>
+      <Text>Nice UI</Text>
       <StatusBar style="auto" />
     </View>
   );
