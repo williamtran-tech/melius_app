51cff2dccf01f9739c399b4ae6546e51ecbcfa84
williamtr4n <williamtran6111@gmail.com>
2023-06-12 15:45:31 +0700

    New era

--- client/App.js
+++ client/App.js
@@ -1,33 +1,15 @@
-import React from "react";
-import { SafeAreaViewBase, StyleSheet, Text, View } from "react-native";
-import BottomNavigation from "./navigation/BottomNavigation";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import OtherNav from "./navigation/OtherNav";
-const Stack = createStackNavigator();
+import { View, Text } from "react-native";
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Main"
-          component={BottomNavigation}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Other"
-          component={OtherNav}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Universal React with Expo</Text>
+    </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
