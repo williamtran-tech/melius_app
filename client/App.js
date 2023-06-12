232b43c0a29e40eeb24005cdf21fd5b49cebac46
Võ Hoàng Tâm <vht1872002@gmail.com>
2023-06-12 18:01:26 +0700

    update IOS cocoapod

--- client/App.js
+++ client/App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import {
+  SafeAreaViewBase,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import { Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+const Stack = createStackNavigator();
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <View style={styles.Btn}>
+        <Image style={[styles.VerticalLogo]} source={VerticalLogo} />
+        <TouchableOpacity
+          style={styles.SignInBtn}
+          onPress={() => console.log("Button pressed")}
+        >
+          <Text style={styles.btnText}>Đăng ký</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.SignUpBtn}
+          onPress={() => console.log("Button pressed")}
+        >
+          <Text style={styles.btnText}>Đăng nhập</Text>
+        </TouchableOpacity>
+        <Text style={{ marginTop: 40 }}>Hoặc đăng nhập bằng</Text>
+        <View style={styles.IconContainer}>
+          <Image
+            style={[styles.icon, { marginRight: 10 }]}
+            source={googleIcon}
+          />
+          <Image
+            style={[styles.icon, { marginRight: 10 }]}
+            source={facebookIcon}
+          />
+          <Image style={styles.icon} source={twitterIcon} />
+        </View>
+      </View> */}
+      <Button
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Press Here</Text>
+      </TouchableOpacity>
+      <Text style={styles.text}>ccc</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
+    color: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    backgroundColor: "#f0f0f0",
   },
 });
