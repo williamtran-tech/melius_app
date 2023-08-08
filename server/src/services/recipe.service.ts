fdba706277d03f8e73f25d944e41e8b96276bcfe
williamtr4n <williamtran6111@gmail.com>
2023-08-08 16:25:21 +0700

    Add search recipe by Id - random recipe - search recipe by name

--- server/src/services/recipe.service.ts
+++ server/src/services/recipe.service.ts
@@ -8,11 +8,10 @@ export default class RecipeService {
   constructor() {}
   public async getRecipes(limit: number) {
     try {
-      console.log("Recipe Service");
       const idErrorRecipe: number[] = [];
       const recipes = await Recipe.findAll({
         limit: limit,
-        attributes: ["id", "name", "nSteps", "nIngredients", "ingredients"],
+        attributes: ["id", "name", "nSteps", "nIngredients", "ingredients", "steps"],
         order: sequelize.literal("rand()"),
       });
 
@@ -21,57 +20,100 @@ export default class RecipeService {
         const stringData = data.replace(/'/g, '"');
         let ingredientsArray: string[] = [];
 
+        const step = recipe.steps;
+        const stringStep = step.replace(/'/g, '"');
+        let stepsArray: string[] = [];
+
         ingredientsArray = JSON.parse(stringData);
+        stepsArray = JSON.parse(stringStep);
 
         return {
+          id: recipe.id,
           name: recipe.name,
           nSteps: recipe.nSteps,
           nIngredients: recipe.nIngredients,
           ingredients: ingredientsArray,
+          steps: stepsArray,
         };
       });
-      // console.log(idErrorRecipe);
-      // for (let i = 0; i < idErrorRecipe.length; i++) {
-      //   const idString = idErrorRecipe[i].toString();
-      //   const invalidIngre = await Recipe.findOne({ where: { id: idString } });
-      //   // console.log("Invalid: ", invalidIngre?.ingredients);
-      //   // console.log(typeof invalidIngre?.ingredients);
-      //   const convertString = JSON.stringify(invalidIngre?.ingredients);
-      //   let parsedArray = convertString.replace("\\", "");
-      //   console.log(parsedArray);
-      //   // parsedArray = '"' + parsedArray.slice(1, -1) + '"';
-      //   // console.log("Parsed: ", parsedArray);
-      //   // parsedArray = parsedArray.replace(/\\|"(\|\")/g, "").replace(/"/g, "'");
-      //   // parsedArray = '"' + parsedArray.slice(1, -1) + '"';
-      //   // console.log("Parsed 1: ", parsedArray);
-      //   // const parsedString = JSON.parse(parsedArray);
-      //   // console.log("Converted: ", JSON.stringify(parsedString));
-      //   // console.log("Updated: ", invalidIngre?.ingredients);
-      //   // console.log("Array: ", JSON.parse(JSON.stringify(parsedString)));
-      //   console.log("Array: ", JSON.parse(JSON.stringify(parsedArray)));
-      //   // const array2string = JSON.stringify(
-      //   //   JSON.parse(JSON.stringify(parsedString))
-      //   // );
-      //   // await Recipe.update(
-      //   //   { ingredients: JSON.stringify(parsedArray) },
-      //   //   { where: { id: idString } }
-      //   // );
-      // }
-
-      // // console.log(recipes[0].name);
-      // // // Get the step string from the recipe;
-      // // const data = recipes[0].steps;
-      // // // Replace all single quotes with double quotes
-      // // const stringData = data.replace(/'/g, '"');
-      // // // Parse the string into an array
-      // // const stepsArray = JSON.parse(stringData);
-
-      // // stepsArray.forEach((step: any) => {
-      // //   console.log(step);
-      // // });
       return responseRecipes;
     } catch (error) {
       console.log(error);
+      throw error;
+    }
+  }
+
+  public async searchRecipes(name: string) {
+    try {
+      const recipes = await Recipe.findAll({
+        where: {
+          name: {
+            [sequelize.Op.like]: `%${name}%`,
+          },
+        },
+        attributes: ["id", "name", "nSteps", "nIngredients", "ingredients", "steps"],
+        limit: 5,
+      });
+
+      const responseRecipes = recipes.map((recipe: any) => {
+        const data = recipe.ingredients;
+        const stringData = data.replace(/'/g, '"');
+        let ingredientsArray: string[] = [];
+
+        const step = recipe.steps;
+        const stringStep = step.replace(/'/g, '"');
+        let stepsArray: string[] = [];
+
+        ingredientsArray = JSON.parse(stringData);
+        stepsArray = JSON.parse(stringStep);
+
+        return {
+          id: recipe.id,
+          name: recipe.name,
+          nSteps: recipe.nSteps,
+          nIngredients: recipe.nIngredients,
+          ingredients: ingredientsArray,
+          steps: stepsArray,
+        };
+      });
+
+      return responseRecipes;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async getRecipeById(id: number) {
+    try {
+      const recipe = await Recipe.findOne({
+        where: { id: id },
+        attributes: ["id", "name", "nSteps", "nIngredients", "ingredients", "steps"],
+      });
+
+      if (recipe) {
+        const data = recipe.ingredients;
+        const stringData = data.replace(/'/g, '"');
+        let ingredientsArray: string[] = [];
+
+        const step = recipe.steps;
+        const stringStep = step.replace(/'/g, '"');
+        let stepsArray: string[] = [];
+
+        ingredientsArray = JSON.parse(stringData);
+        stepsArray = JSON.parse(stringStep);
+
+        return {
+          id: recipe.id,
+          name: recipe.name,
+          nSteps: recipe.nSteps,
+          nIngredients: recipe.nIngredients,
+          ingredients: ingredientsArray,
+          steps: stepsArray,
+        };
+      }
+
+      return recipe;
+    } catch (error) {
       throw error;
     }
   }
